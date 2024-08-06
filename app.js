836d53a566390de62a027238a1573eb00f9ee38b
RoyWang <66111562+lanni0619@users.noreply.github.com>
2024-08-06 10:00:44 +0800

    security
    - Sending JWT via Cookie
    - Implement Rate Limiting
    - Security HTTP Headers
    - Data Sanitization
    - Preventing HTTP parameter pollution

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xssClean = require('xss-clean');
+const hpp = require('hpp');
+
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 const AppError = require('./utils/appError');
@@ -7,18 +13,54 @@ const errorController = require('./controllers/errorController');
 
 const app = express();
 
+// ---------- Global middlewares ----------
+
+// Set Security HTTP Header
+app.use(helmet());
+
+// Development Logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
 
-app.use(express.json());
+// rate limiting
+const limiter = rateLimit({
+  windowMs: 60 * 60 * 1000, // 60 minutes
+  limit: 100,
+  message: 'To many requests from this IP. Please try again in an 1 hour.',
+});
+app.use('/api', limiter);
 
-// Use static file
+// Body parser: Ready data from body & into req.body object
+app.use(express.json({ limit: '10kb' }));
+
+// Data sanitization against NoSQL query injection
+app.use(mongoSanitize());
+// Data sanitization against XSS
+app.use(xssClean());
+// HTTP params pollution
+app.use(
+  hpp({
+    // allowed duplicate query string
+    whitelist: [
+      'duration',
+      'ratingsQuantity',
+      'ratingsAverage',
+      'maxGroupSize',
+      'difficulty',
+      'price',
+    ],
+  }),
+);
+
+// Serving static file
 // app.use(express.static(`${__dirname}/public`));
 
+// Self-define middleware
 app.use((req, res, next) => {
   // define a requestTime Object
   req.requestTime = new Date().toISOString();
+  // console.log(req.header);
 
   next();
 });
