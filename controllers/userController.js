2201bde01d55852e9955e1557b966cf8e2eb6ccc
RoyWang <66111562+lanni0619@users.noreply.github.com>
2024-08-29 19:11:30 +0800

    Upload Image
    - Build Upload image function by multer & sharp package
    - Build upload user's portrait function in userController & website
    - Build upload tour's cover & images in tourController

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,58 @@
+const multer = require('multer');
+const sharp = require('sharp');
 const User = require('../models/userModel');
 const AppError = require('../utils/appError');
 const catchAsync = require('../utils/catchAsync');
 const factory = require('./handlerFactory');
+
+// multer option: diskStorage
+
+// Store at disk
+// const multerStorage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     cb(null, 'public/img/users');
+//   },
+//   filename: function (req, file, cb) {
+//     const extension = file.mimetype.split('/')[1];
+//     cb(null, `user-${req.user.id}-${Date.now()}.${extension}`);
+//   },
+// });
+
+// Store as a buffer: keep image in the memory
+const multerStorage = multer.memoryStorage();
+
+// This middleware is running after photo uploaded
+exports.resizeUserPhoto = catchAsync(async (req, res, next) => {
+  if (!req.file) return next();
+
+  req.file.filename = `user-${req.user.id}-${Date.now()}.jpeg`;
+
+  // Use sharp package to resize image
+  // https://sharp.pixelplumbing.com/api-resize
+  await sharp(req.file.buffer)
+    .resize(500, 500)
+    .toFormat('jpeg')
+    .jpeg({ quality: 90 })
+    .toFile(`public/img/users/${req.file.filename}`);
+
+  next();
+});
+
+// multer option: filter
+const multerFilter = (req, file, cb) => {
+  if (file.mimetype.startsWith('image')) {
+    cb(null, true);
+  } else {
+    cb(new AppError('Not an image! Please upload only images.', 400), false);
+  }
+};
+
+const upload = multer({
+  storage: multerStorage,
+  fileFilter: multerFilter,
+});
+
+exports.uploadUserPhoto = upload.single('photo');
 
 // Use in UpdateMe
 const filterObj = (reqBodyObj, ...allowedFields) => {
@@ -26,6 +77,7 @@ exports.updateMe = catchAsync(async (req, res, next) => {
 
   // 2) Filtered out unwanted fields
   const filterBody = filterObj(req.body, 'name', 'email');
+  if (req.file) filterBody.photo = req.file.filename;
 
   // 3) Update user document
   const updatedUser = await User.findByIdAndUpdate(req.user.id, filterBody, {
--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -8,9 +8,9 @@ router.get('/', authController.isLoggedIn, viewController.getOverview);
 router.get('/tour/:slug', authController.isLoggedIn, viewController.getTour);
 router.get('/login', authController.isLoggedIn, viewController.getloginForm);
 router.get('/me', authController.protect, viewController.getAccount);
-router.post(
-  '/submit-user-data',
-  authController.protect,
-  viewController.updateUserData,
-);
+// router.post(
+//   '/submit-user-data',
+//   authController.protect,
+//   viewController.updateUserData,
+// );
 module.exports = router;
