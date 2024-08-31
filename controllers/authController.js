09c58dc10edac20f60412f8093e1f9bd8374755e
RoyWang <66111562+lanni0619@users.noreply.github.com>
2024-08-31 13:44:33 +0800

    email handler (#17)
    - Refactoring utils/email.js to a Email class
    - Call new Email(user, subject).sendWelcome( ) when signed up successfully
    - Call new Email(user, subject).sendResetPassword( ) when the endpoint of forgotPassword was hitted
    - Use different transport server in dev & prod mode.

--- controllers/authController.js
+++ controllers/authController.js
@@ -7,7 +7,7 @@ const User = require('../models/userModel');
 // Functions
 const catchAsync = require('../utils/catchAsync');
 const AppError = require('../utils/appError');
-const sendEmail = require('../utils/email');
+const Email = require('../utils/email');
 
 const signToken = (id) => {
   return jwt.sign({ id }, process.env.JWT_SECRET, {
@@ -53,6 +53,12 @@ exports.signup = catchAsync(async (req, res, next) => {
     role,
   });
   newUser.password = undefined;
+
+  const url = `${req.protocol}://${req.get('host')}/me`;
+  console.log({ url });
+
+  await new Email(newUser, url).sendWelcome();
+
   createSendToken(newUser, 201, res);
 });
 
@@ -161,7 +167,7 @@ exports.isLoggedIn = async (req, res, next) => {
   next();
 };
 
-// roles = ['admin','lead-guide' ...] we can extend it.
+// roles = ['admin','lead-guide' ...] that we can extend it.
 exports.restrictTo = (...roles) => {
   return (req, res, next) => {
     // if user's role = user
@@ -177,10 +183,9 @@ exports.restrictTo = (...roles) => {
 
 // Password Reset Functionality
 exports.forgotPassword = catchAsync(async (req, res, next) => {
-  // 1) Get User From Poseted Email
-  const { email } = req.body;
+  // 1) Get User From POSTed Email
 
-  const user = await User.findOne(email);
+  const user = await User.findOne({ email: req.body.email });
 
   if (!user) return next(new AppError('The user is not exist!', 404));
 
@@ -190,15 +195,11 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
   await user.save({ validateBeforeSave: false });
 
   // 3) Send email to user
-  const resetURL = `${req.protocol}://${req.get('host')}/api/v1/users/resetPassword/${resetToken}`;
-  const message = `Forgot your password? Submit a PATCH request with your new password and passwordConfirm to :${resetURL}.\nIf you didn't forget your password, please ignore this email!`;
 
   try {
-    await sendEmail({
-      email: user.email,
-      subject: 'Your password reset token (valid for 10 min)',
-      message,
-    });
+    const resetURL = `${req.protocol}://${req.get('host')}/api/v1/users/resetPassword/${resetToken}`;
+    await new Email(user, resetURL).sendResetPassword();
+
     res.status(200).json({
       status: 'success',
       message: 'Token sent to email!',
--- utils/email.js
+++ utils/email.js
@@ -1,27 +1,71 @@
 const nodemailer = require('nodemailer');
+const pug = require('pug');
+const htmlToText = require('html-to-text');
 
-const sendEmail = async (options) => {
-  // 1) Create a transporter
-  const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
-    secure: false, // Use `true` for port 465, `false` for all other ports
-    auth: {
-      user: process.env.EMAIL_USERNAME,
-      pass: process.env.EMAIL_PASSWORD,
-    },
-  });
-  // 2) Define the email options
-  const emailOptions = {
-    from: '"Roy Wang 🤠" <lanni0619@gmail.com>',
-    to: options.email,
-    subject: options.subject,
-    text: options.message,
-    // html:
-  };
+// new Email(user, url).sendWelcome()
 
-  // 3) Actually send the email
-  await transporter.sendMail(emailOptions);
+module.exports = class Email {
+  constructor(user, url) {
+    this.to = user.email;
+    this.firstName = user.name.split(' ')[0];
+    this.url = url;
+    this.from = `Roy Wang🤠 <${process.env.EMAIL_FROM}>`;
+  }
+
+  newTransport() {
+    if (process.env.NODE_ENV === 'development') {
+      return nodemailer.createTransport({
+        host: process.env.EMAIL_HOST,
+        port: process.env.EMAIL_PORT,
+        secure: false, // Use `true` for port 465, `false` for all other ports
+        auth: {
+          user: process.env.EMAIL_USERNAME,
+          pass: process.env.EMAIL_PASSWORD,
+        },
+      });
+    }
+    // Production Mode: Brevo Transport
+    return nodemailer.createTransport({
+      service: 'Brevo',
+      host: process.env.BREVO_SMTP_SERVER,
+      port: process.env.BREVO_SMTP_PORT,
+      auth: {
+        user: process.env.BREVO_SMTP_LOGIN,
+        pass: process.env.BREVO_SMTP_APIKEY,
+      },
+    });
+  }
+
+  async send(template, subject) {
+    // 1) Render HTML based on a pug template
+    const html = pug.renderFile(
+      `${__dirname}/../views/emails/${template}.pug`,
+      {
+        firstName: this.firstName,
+        url: this.url,
+        subject,
+      },
+    );
+
+    // 2) Define email option
+    const emailOptions = {
+      from: this.from,
+      to: this.to,
+      subject,
+      html,
+      text: htmlToText.convert(html),
+    };
+    console.log(this.to);
+
+    // 3) Create a transport & send email
+    await this.newTransport().sendMail(emailOptions);
+  }
+
+  sendWelcome() {
+    this.send('welcome', 'Hello & Welcome to the family 😁');
+  }
+
+  sendResetPassword() {
+    this.send('ResetPassword', 'Your password reset token (valid for 10 min)');
+  }
 };
-
-module.exports = sendEmail;
